f40415b3cf61b28855f650ec1805f43f71a959ac
VictorGenario <victordamatagenario@gmail.com>
2022-06-13 17:18:30 -0300

    Subscribe Button add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d27dda53141dda27"; }
+/******/ 		__webpack_require__.h = function() { return "0054d91904005d22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
