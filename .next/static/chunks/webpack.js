f4c8aa1c6b789f1082d961d5c8d8bacf3fe3e090
VictorGenario <victordamatagenario@gmail.com>
2022-08-02 19:38:16 -0300

    Config stripe subscribe

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17f54d5e09944e0a"; }
+/******/ 		__webpack_require__.h = function() { return "73f982777f4a5a7a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
